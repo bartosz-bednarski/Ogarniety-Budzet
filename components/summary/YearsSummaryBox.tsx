90e59e2cd155171acf18b0e8e21c7677e18dd94b
Bartosz Bednarski <bambocha1997@gmail.com>
2024-01-31 15:33:10 +0100

    Updated epenses grayBoxes.

---
+++ components/summary/YearsSummaryBox.tsx
@@ -0,0 +1,128 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import COLORS_STYLE from "../../utils/styles/colors";
+import PieChart from "react-native-pie-chart";
+import { Ionicons } from "@expo/vector-icons";
+import { numberWithSpaces } from "../../utils/numberWithSpaces";
+const YearsSummaryBox: React.FC<{
+  data: { year: number; expenses: number; incomes: number; savings: number };
+}> = ({ data }) => {
+  const pieChartData = [
+    data.incomes !== 0
+      ? Number(((data.expenses / data.incomes) * 100).toFixed(2))
+      : 1,
+    100 - Number(((data.expenses / data.incomes) * 100).toFixed(2)) < 0
+      ? 0
+      : 100 - Number(((data.expenses / data.incomes) * 100).toFixed(2)),
+  ];
+  return (
+    <View style={styles.container}>
+      <Text style={styles.monthName}>{data.year}</Text>
+      <View style={styles.box}>
+        <View style={styles.mainBox}>
+          <View style={styles.chartBox}>
+            <PieChart
+              widthAndHeight={120}
+              series={pieChartData}
+              sliceColor={["red", "green"]}
+              coverRadius={0.45}
+              coverFill={COLORS_STYLE.tabGrey}
+            />
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.whiteLabel}>Przychody</Text>
+            <View style={styles.rowText}>
+              <Ionicons
+                name="trending-up-outline"
+                color={COLORS_STYLE.basicGold}
+                size={16}
+              />
+              <Text style={styles.value}>
+                {numberWithSpaces(data.incomes)} PLN
+              </Text>
+            </View>
+            <Text style={styles.whiteLabel}>Wydatki</Text>
+            <View style={styles.rowText}>
+              <Ionicons
+                name="trending-down-outline"
+                color={COLORS_STYLE.basicGold}
+                size={16}
+              />
+              <Text style={styles.value}>
+                {numberWithSpaces(data.expenses)} PLN
+              </Text>
+            </View>
+            <Text style={styles.whiteLabel}>Oszczędności</Text>
+            <View style={styles.rowText}>
+              <Ionicons
+                name="wallet-outline"
+                color={COLORS_STYLE.basicGold}
+                size={16}
+              />
+              <Text style={styles.value}>
+                {numberWithSpaces(data.savings)} PLN
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    gap: 5,
+  },
+  box: {
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    borderRadius: 15,
+    width: "100%",
+    backgroundColor: COLORS_STYLE.tabGrey,
+  },
+  monthName: {
+    fontSize: 20,
+    color: "white",
+    marginLeft: 5,
+  },
+  mainBox: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 20,
+  },
+  chartBox: {
+    width: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textWhiteBig: {
+    width: "100%",
+    fontSize: 26,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "600",
+  },
+  details: {
+    width: "60%",
+  },
+  value: {
+    fontSize: 24,
+    textAlign: "left",
+    color: COLORS_STYLE.basicGold,
+  },
+  rowText: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+    marginVertical: 5,
+    borderBottomWidth: 1,
+    borderColor: COLORS_STYLE.basicGold,
+  },
+  whiteLabel: {
+    color: "white",
+    fontSize: 10,
+  },
+});
+export default YearsSummaryBox;
--- screens/summary/YearsSummaryScreen.tsx
+++ screens/summary/YearsSummaryScreen.tsx
@@ -1,17 +1,123 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import COLORS_STYLE from "../../utils/styles/colors";
+import { useAppSelector } from "../../redux/hooks";
+import GoldenFrame from "../../utils/ui/GoldenFrame";
+import PieChart from "react-native-pie-chart";
+import GrayBox50 from "../../utils/ui/GrayBox50";
+import GrayBox100 from "../../utils/ui/GrayBox100";
+import pieChartColors from "../../utils/styles/pieChartColors";
+import YearsSummaryBox from "../../components/summary/YearsSummaryBox";
 const YearsSummaryScreen = () => {
-  return <View style={styles.container}></View>;
+  const yearsExpenses = useAppSelector((state) => state.expenses.yearsExpenses);
+  const yearsIncomes = useAppSelector((state) => state.incomes.yearsIncomes);
+  const yearsSavings = useAppSelector((state) => state.piggyBank.yearsSavings);
+  const sumOfAllIncomes = yearsIncomes
+    .map((item) => item.sumOfAllIncomes)
+    .reduce((partialSum, a) => partialSum + a, 0);
+  const sumOfAllExpenses = yearsExpenses
+    .map((item) => item.sumOfAllExpenses)
+    .reduce((partialSum, a) => partialSum + a, 0);
+  const sumOfAllSavings = yearsSavings
+    .map((item) => item.sumOfSavings)
+    .reduce((partialSum, a) => partialSum + a, 0);
+  const pieChartData = [
+    sumOfAllIncomes !== 0
+      ? Number(((sumOfAllExpenses / sumOfAllIncomes) * 100).toFixed(2))
+      : 1,
+    100 - Number(((sumOfAllExpenses / sumOfAllIncomes) * 100).toFixed(2)) < 0
+      ? 0
+      : 100 - Number(((sumOfAllExpenses / sumOfAllIncomes) * 100).toFixed(2)),
+  ];
+  const grayBoxData = yearsExpenses.map((item) => ({
+    year: item.year,
+    expenses: item.sumOfAllExpenses,
+    incomes:
+      yearsIncomes[yearsIncomes.findIndex((k) => k.year === item.year)]
+        .sumOfAllIncomes,
+    savings:
+      yearsSavings[yearsSavings.findIndex((k) => k.year === item.year)]
+        .sumOfSavings,
+  }));
+  console.log("expense", yearsExpenses);
+  console.log("incomes", yearsIncomes);
+  console.log("savings", yearsSavings);
+  console.log(grayBoxData);
+  return (
+    <ScrollView style={styles.container}>
+      {yearsIncomes.length === 0 && (
+        <View style={styles.informationBox}>
+          <Text style={styles.informationText}>
+            Tutaj wyświetlane będą informacje o przychodach z poszczególnych lat
+          </Text>
+        </View>
+      )}
+      {yearsIncomes.length > 0 && (
+        <>
+          <GrayBox100 name="Oszczędzono" value={sumOfAllSavings} />
+          <View style={styles.pieChart}>
+            <PieChart
+              widthAndHeight={200}
+              series={pieChartData}
+              sliceColor={["red", "green"]}
+              coverRadius={0.45}
+              coverFill={COLORS_STYLE.backgroundBlack}
+            />
+          </View>
+          <View style={styles.rowBox}>
+            <GrayBox50 name="Przychody" value={sumOfAllIncomes} />
+            <GrayBox50 name="Wydatki" value={sumOfAllExpenses} />
+          </View>
+          {grayBoxData.map((item) => (
+            <YearsSummaryBox data={item} key={item.year} />
+          ))}
+
+          {/* <View style={styles.monthIncomesBox}>
+            {yearsIncomes.map((year) => (
+              <YearIncomesBox yearIncomes={year} key={year.year} />
+            ))}
+          </View> */}
+        </>
+      )}
+    </ScrollView>
+  );
 };
 const styles = StyleSheet.create({
+  informationBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 400,
+  },
+  informationText: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "white",
+  },
   container: {
     flex: 1,
-    paddingVertical: 10,
+    marginVertical: 10,
     paddingHorizontal: 20,
     backgroundColor: COLORS_STYLE.backgroundBlack,
   },
   text: {
     color: "white",
+  },
+  rowBox: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    marginVertical: 10,
+  },
+  pieChart: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 200,
+    marginVertical: 20,
+  },
+  monthIncomesBox: {
+    gap: 15,
   },
 });
 export default YearsSummaryScreen;
